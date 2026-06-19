260152d173e18be021f463814e09992fc60fd18f
Vignesh <vigneshvicky8888vicky@gmail.com>
2026-06-19 09:13:39 +0530

    19/6/2026

---
+++ fourfour.py
@@ -0,0 +1,57 @@
+#odd or even
+'''num=int(input("enter the number:"))
+if num%2==0:
+    print("odd number then pa")
+else:
+    print("even number")
+#user input mlyiplay
+num=int(input("enter thr multiplay number"))
+if num%2==0 and num%3==0:
+    print("divisable")
+else:
+    print("not divisable")
+#multiplay
+num=int(input("enter the  number"))
+if num%5==0:
+    print("diviable")
+else:
+    print("not divisable")'''
+#leep year
+year=int(input("enter thr year"))
+if year%4==0:
+    if year%100==0:
+         if year%400==0:
+             print("leap year")
+         else:
+             print("not leap year")
+    else:
+        print("leap year")
+else:
+    print("not leap year")
+#studentmark list
+mark=int(input("enna pa unaku marku uhh"))
+if mark>=95:
+    print("A++")
+elif mark>=85:
+    print("A+")
+elif mark>=80:
+    print("B")
+elif mark>=75:
+    print("c")
+elif mark>=65:
+    print("D+")
+elif mark>=50:
+    print("D")
+elif mark<=34:
+    print("FAIL")
+else:
+    print("FAIL")
+#perusu chinathu
+
+num=int(input("ethachum number kudu ya"))
+if num>0==0:
+    print("posative")
+elif num<0==0:
+    print("negative")
+else:
+    print("zero")
