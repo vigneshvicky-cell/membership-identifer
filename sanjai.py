d02bc94321fce832cf5fb9f6d0ab0dd6cfee3fb2
Vignesh <vigneshvicky8888vicky@gmail.com>
2026-06-22 19:02:32 +0530

    Add files via upload

---
+++ sanjai.py
@@ -0,0 +1,54 @@
+'''#upper case
+name=input("enter the name:")
+print(name.upper())
+#lower case
+name=input("enter the name:")
+print(name.lower())
+#first letter
+name=input("enter the name:")
+print(name.capitalize())
+'''
+"""
+#
+name=input("enter the name:")
+age=int(input("enter the age:"))
+print("My name is {name} and i am {age} years old.")
+"""
+"""
+#
+word=input("enter the word:")
+char=input("enter the charatert:")
+print(word.index(char))
+"""
+"""
+#
+name=input("enter the name:")
+sub=input("enter the substrin:")
+print(name.find(sub))
+"""
+"""
+#
+name=input("enter the name:")
+print(name.endswith("esh"))
+"""
+"""
+#
+name="name\tAge\tCity"
+print(name)
+print(name.expandtabs(10))
+"""
+"""#
+name=input("enter the string:")
+print(name.isdigit())
+"""
+#
+"""name=input("enter the string:")
+print(name.isnumeric())
+"""
+#
+name=input("enter the string:")
+print(name.isascii())
+#
+name=input("enter the string:")
+print(name.isalpha())
+#
